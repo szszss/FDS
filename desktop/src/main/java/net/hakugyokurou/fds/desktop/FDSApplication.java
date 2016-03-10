7ff8a14e73cfbd3d3351c770d740c2b2c090912b
szszss <772619266@qq.com>
2016-03-11 00:46:48 +0800

    Update on 2016-3-11

--- desktop/src/main/java/net/hakugyokurou/fds/desktop/FDSApplication.java
+++ desktop/src/main/java/net/hakugyokurou/fds/desktop/FDSApplication.java
@@ -18,6 +18,8 @@ import javax.swing.JLabel;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -28,13 +30,21 @@ import java.util.ArrayList;
 import javax.swing.JTextField;
 import javax.swing.JMenuItem;
 import javax.swing.JMenu;
+import java.awt.FlowLayout;
+import org.eclipse.wb.swing.FocusTraversalOnArray;
+import java.awt.Component;
+import javax.swing.SwingConstants;
+import javax.swing.JScrollPane;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class FDSApplication {
 
 	private JFrame frame;
-	private JTable table;
 	private JTextField textField;
 	private JTextField textField_1;
+	private JTable table;
+	private JLabel lblResult;
 
 	/**
 	 * Launch the application.
@@ -67,48 +77,28 @@ public class FDSApplication {
 		frame.setBounds(100, 100, 500, 700);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JMenuBar menuBar = new JMenuBar();
-		frame.setJMenuBar(menuBar);
-		
-		JMenu mnLoad = new JMenu("Load");
-		menuBar.add(mnLoad);
-		
-		JMenu mnGen = new JMenu("Generator");
-		menuBar.add(mnGen);
-		
-		JMenu mnParse = new JMenu("Parse");
-		menuBar.add(mnParse);
-		
-		table = new JTable();
-		table.setModel(new DefaultTableModel(
-			new Object[][] {
-			},
-			new String[] {
-				"Question", "Answer"
-			}
-		));
-		frame.getContentPane().add(table, BorderLayout.CENTER);
-		
 		JPanel panel = new JPanel();
 		frame.getContentPane().add(panel, BorderLayout.SOUTH);
 		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[]{0, 0, 0};
+		gbl_panel.columnWidths = new int[] {0, 0, 0};
 		gbl_panel.rowHeights = new int[]{0, 0, 0, 0};
 		gbl_panel.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
 		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
 		panel.setLayout(gbl_panel);
 		
 		JLabel lblNewLabel = new JLabel("Question");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
 		gbc_lblNewLabel.anchor = GridBagConstraints.EAST;
-		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.insets = new Insets(5, 0, 5, 5);
 		gbc_lblNewLabel.gridx = 0;
 		gbc_lblNewLabel.gridy = 0;
 		panel.add(lblNewLabel, gbc_lblNewLabel);
 		
 		textField = new JTextField();
+		textField.setEditable(false);
 		GridBagConstraints gbc_textField = new GridBagConstraints();
-		gbc_textField.insets = new Insets(0, 0, 5, 0);
+		gbc_textField.insets = new Insets(5, 0, 5, 5);
 		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField.gridx = 1;
 		gbc_textField.gridy = 0;
@@ -116,6 +106,7 @@ public class FDSApplication {
 		textField.setColumns(10);
 		
 		JLabel lblNewLabel_1 = new JLabel("Answer");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
 		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
 		gbc_lblNewLabel_1.anchor = GridBagConstraints.EAST;
 		gbc_lblNewLabel_1.insets = new Insets(0, 0, 0, 5);
@@ -125,6 +116,7 @@ public class FDSApplication {
 		
 		textField_1 = new JTextField();
 		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.insets = new Insets(0, 0, 0, 5);
 		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField_1.gridx = 1;
 		gbc_textField_1.gridy = 1;
@@ -133,17 +125,57 @@ public class FDSApplication {
 		
 		JButton btnNewButton = new JButton("Check");
 		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
-		gbc_btnNewButton.insets = new Insets(0, 0, 5, 5);
+		gbc_btnNewButton.insets = new Insets(5, 15, 5, 5);
 		gbc_btnNewButton.gridx = 0;
 		gbc_btnNewButton.gridy = 2;
 		panel.add(btnNewButton, gbc_btnNewButton);
 		
-		JLabel lblNewLabel_2 = new JLabel("");
-		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
-		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 0);
-		gbc_lblNewLabel_2.gridx = 1;
-		gbc_lblNewLabel_2.gridy = 2;
-		panel.add(lblNewLabel_2, gbc_lblNewLabel_2);
+		lblResult = new JLabel("");
+		GridBagConstraints gbc_lblResult = new GridBagConstraints();
+		gbc_lblResult.insets = new Insets(0, 0, 5, 5);
+		gbc_lblResult.gridx = 1;
+		gbc_lblResult.gridy = 2;
+		panel.add(lblResult, gbc_lblResult);
+		panel.setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{btnNewButton, lblNewLabel, textField, lblNewLabel_1, textField_1, lblResult}));
+		
+		JPanel panel_1 = new JPanel();
+		frame.getContentPane().add(panel_1, BorderLayout.NORTH);
+		panel_1.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
+		
+		JButton btnLoad = new JButton("Load");
+		panel_1.add(btnLoad);
+		
+		JButton btnGenerate = new JButton("Generate");
+		btnGenerate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Generator generator = new Generator();
+				generator.setModal(true);
+				generator.setVisible(true);
+				if(generator.ok)
+				{
+					ArrayList<MathExpression> expressions = generator.expressions;
+				}
+				generator.dispose();
+			}
+		});
+		panel_1.add(btnGenerate);
+		
+		JButton btnParse = new JButton("Parse");
+		panel_1.add(btnParse);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"Question", "Answer"
+			}
+		));
+		table.getColumnModel().getColumn(0).setPreferredWidth(300);
+		scrollPane.setViewportView(table);
 	}
 
 	public static ArrayList<MathExpression> loadFromFile(File file) throws InvalidExpressionException, IOException {
---
+++ desktop/src/main/java/net/hakugyokurou/fds/desktop/Generator.java
@@ -0,0 +1,149 @@
+package net.hakugyokurou.fds.desktop;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import net.hakugyokurou.fds.MathExpression;
+import net.hakugyokurou.fds.generator.MathExpressionGenerator;
+
+import javax.swing.JTextField;
+import javax.swing.JRadioButton;
+import javax.swing.JLabel;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.awt.event.ActionEvent;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import java.awt.Dialog.ModalExclusionType;
+import java.awt.Dialog.ModalityType;
+
+class Generator extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	ArrayList<MathExpression> expressions = new ArrayList<MathExpression>();
+	boolean ok = false;
+	private JRadioButton rdbtnEasy;
+	private JRadioButton rdbtnNormal;
+	private JRadioButton rdbtnHard;
+	private JRadioButton rdbtnLunatic;
+	private JSpinner spinner;
+	private JTextArea textArea;
+	private JButton okButton;
+
+	public Generator() {
+		setModal(true);
+		setModalityType(ModalityType.TOOLKIT_MODAL);
+		setTitle("Expression Generator");
+		setBounds(100, 100, 500, 550);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(new BorderLayout(0, 0));
+		{
+			JPanel panel = new JPanel();
+			contentPanel.add(panel, BorderLayout.SOUTH);
+			ButtonGroup group = new ButtonGroup();
+			panel.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
+			{
+				rdbtnEasy = new JRadioButton("Easy");
+				group.add(rdbtnEasy);
+				panel.add(rdbtnEasy);
+			}
+			{
+				rdbtnNormal = new JRadioButton("Normal");
+				rdbtnNormal.setSelected(true);
+				group.add(rdbtnNormal);
+				panel.add(rdbtnNormal);
+			}
+			{
+				rdbtnHard = new JRadioButton("Hard");
+				group.add(rdbtnHard);
+				panel.add(rdbtnHard);
+			}
+			{
+				rdbtnLunatic = new JRadioButton("Lunatic");
+				group.add(rdbtnLunatic);
+				panel.add(rdbtnLunatic);
+			}
+			{
+				JLabel lblCount = new JLabel("Count:");
+				panel.add(lblCount);
+			}
+			{
+				spinner = new JSpinner();
+				spinner.setModel(new SpinnerNumberModel(20, 1, 100, 1));
+				panel.add(spinner);
+			}
+			{
+				JButton btnGege = new JButton("Generate");
+				btnGege.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						if(rdbtnEasy.isSelected()) {
+							expressions = MathExpressionGenerator.generateEasy(((Number)spinner.getValue()).intValue());
+						} else if(rdbtnNormal.isSelected()) {
+							expressions = MathExpressionGenerator.generateNormal(((Number)spinner.getValue()).intValue());
+						} else if(rdbtnHard.isSelected()) {
+							expressions = MathExpressionGenerator.generateHard(((Number)spinner.getValue()).intValue());
+						} else if(rdbtnLunatic.isSelected()) {
+							expressions = MathExpressionGenerator.generateLunatic(((Number)spinner.getValue()).intValue());
+						}
+						StringBuilder sb = new StringBuilder();
+						for(MathExpression expr : expressions) 
+						{
+							sb.append(expr).append("\n");
+						}
+						textArea.setText(sb.toString());
+						okButton.setEnabled(true);
+					}
+				});
+				panel.add(btnGege);
+			}
+		}
+		{
+			JScrollPane scrollPane = new JScrollPane();
+			contentPanel.add(scrollPane, BorderLayout.CENTER);
+			{
+				textArea = new JTextArea();
+				textArea.setEditable(false);
+				scrollPane.setViewportView(textArea);
+			}
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				okButton = new JButton("OK");
+				okButton.setEnabled(false);
+				okButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						ok = true;
+						setVisible(false);
+					}
+				});
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						setVisible(false);
+					}
+				});
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+
+}
